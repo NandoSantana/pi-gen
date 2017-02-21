022f9993c3d501ce3690503cd03bed2a069a9f02
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-21 15:43:43 +0100

    Try to start Docker daemon

--- stage2/02-docker-pull/01-run.sh
+++ stage2/02-docker-pull/01-run.sh
@@ -2,6 +2,9 @@
 
 on_chroot << EOF
 echo 'Pulling Docker images'
+systemctl start docker
 docker pull plossys/blinkt
 docker pull sealsystems/visualizer:latest-arm
+systemctl stop docker
+rm -f /etc/docker/key.json
 EOF
