ff7e9521b77c97de7df8f3c0c99258ece50df640
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-21 17:07:17 +0100

    Show more details

--- stage2/02-docker-pull/01-run.sh
+++ stage2/02-docker-pull/01-run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-echo 'Pulling Docker images'
+echo 'Showing Docker version'
+docker version
+echo 'Starting Docker'
 systemctl start docker
+echo 'Showing Docker version'
+docker version
+echo 'Showing processes'
+ps -ef
+echo 'Pulling Docker images'
 docker pull plossys/blinkt
 docker pull sealsystems/visualizer:latest-arm
 systemctl stop docker
