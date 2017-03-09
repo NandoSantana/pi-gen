8ff88fe256c35a0807be484e22e4780b934f59d2
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-09 21:29:51 +0100

    Improve set default locale

--- stage2/02-docker/01-run.sh
+++ stage2/02-docker/01-run.sh
@@ -6,7 +6,10 @@ curl -SsL https://get.docker.com | sh
 echo 'Installing Docker Compose'
 pip install docker-compose
 
+# set default locales to 'en_US.UTF-8'
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
 
-locale-gen "en_US.UTF-8"
-dpkg-reconfigure locales
+echo 'locales locales/default_environment_locale select en_US.UTF-8' | debconf-set-selections
+dpkg-reconfigure -f noninteractive locales
 EOF
