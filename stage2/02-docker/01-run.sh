22ca0f3d9f710a787150182bdd178327ce5d74be
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-09 20:53:53 +0100

    Revert package, use locale-gen

--- stage2/02-docker/01-run.sh
+++ stage2/02-docker/01-run.sh
@@ -5,4 +5,8 @@ echo 'Installing Docker'
 curl -SsL https://get.docker.com | sh
 echo 'Installing Docker Compose'
 pip install docker-compose
+
+
+locale-gen "en_US.UTF-8"
+dpkg-reconfigure locales
 EOF
