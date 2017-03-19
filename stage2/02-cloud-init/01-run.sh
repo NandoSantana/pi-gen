4127b2de8ea993bd0b79b86f9b8d52dd64551dad
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-19 11:40:37 +0100

    Try cloud-init nocloud folder before networking

--- stage2/02-cloud-init/01-run.sh
+++ stage2/02-cloud-init/01-run.sh
@@ -2,7 +2,7 @@
 
 on_chroot << EOF
 echo 'Symlinking cloud-init nocloud-net seed files to /boot partition'
-mkdir -p /var/lib/cloud/seed/nocloud-net
-ln -s /boot/user-data /var/lib/cloud/seed/nocloud-net/user-data
-ln -s /boot/meta-data /var/lib/cloud/seed/nocloud-net/meta-data
+mkdir -p /var/lib/cloud/seed/nocloud
+ln -s /boot/user-data /var/lib/cloud/seed/nocloud/user-data
+ln -s /boot/meta-data /var/lib/cloud/seed/nocloud/meta-data
 EOF
